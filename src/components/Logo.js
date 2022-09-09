9c369a1bdec426548f23772bb07695717de4b474
drakarisbelen <kari.belen@gmail.com>
2022-09-09 01:28:42 -0300

    Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/FreeCodeCamp_logo.png"
+const Logo = () => (
+    <div className="freecodecamp-logo-container">
+         <img
+          src={logo}
+          className="freecodecamp-logo"
+        />
+    </div>
+);
+
+export default Logo;
