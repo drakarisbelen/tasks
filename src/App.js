5f3f31d031d229c70e44b1d2b72184fb0efbd6d3
drakarisbelen <kari.belen@gmail.com>
2022-09-09 02:45:39 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-
+import Logo from "../src/components/Logo"
+import TaskList from "./components/TaskList";
 function App() {
   return (
-    <div className="App">
-     
+    <div className="task-application">
+     <Logo/>
+     <div className='main-task-list'>
+        <h1>Tasks</h1>
+        {/* <Task text="Aprender react" complete={false}/> */}
+        {/* <TaskForm/> */}
+        <TaskList></TaskList>
+     </div>
     </div>
   );
 }
